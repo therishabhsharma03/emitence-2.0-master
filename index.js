04fefdd4b2836f8bb87d3d304af1fd6f33f4802b
rishabhsssharma <manyrishabh@gmail.com>
2023-06-30 00:11:53 +0530

    mail

--- index.js
+++ index.js
@@ -58,7 +58,7 @@ app.post('/submit', (req, res) => {
   // Define the email options
   const mailOptions = {
     from: 'apicheck11@gmail.com',
-    to: 'therishabhsharma03@gmail.com', // Replace with the admin email address
+    to: 'emitencerealty@gmail.com', // Replace with the admin email address
     subject: 'New Flat Enquiry',
     html: emailContent
   };
