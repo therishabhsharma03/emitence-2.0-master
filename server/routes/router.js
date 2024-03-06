148df1ea1eadc253776ae7b855504dd5343d1946
Rishabh Sharma <113721437+therishabhsharma03@users.noreply.github.com>
2024-03-06 12:46:21 +0530

    Update router.js

--- server/routes/router.js
+++ server/routes/router.js
@@ -42,7 +42,7 @@ route.post('/submit', (req, res) => {
     // Define the email options
     const mailOptions = {
       from: 'apicheck11@gmail.com',
-      to: 'emitencerealty@gmail.com', // Replace with the admin email address
+      to: 'admin@emitencerealty.com', // Replace with the admin email address
       subject: 'New Flat Enquiry',
       html: emailContent
     };
